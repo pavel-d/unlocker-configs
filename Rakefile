3f121fd5fb3c23fcd30815c8c1efcbbe1c634c1e
Pavel Dotsulenko <pavel@assembla.com>
2017-08-13 15:58:44 +0300

    Update hosts

--- Rakefile
+++ Rakefile
@@ -1,17 +1,11 @@
 require 'erb'
 require 'yaml'
-require 'simpleidn'
 
 DYMAMIC_CONFIGS = %w(config/smart-proxy/settings.yml config/bind/zones.override)
 
 task :render_config do
-  additional_host_names = YAML.load(File.read('./config/settings.yml'))['proxy_hosts']
+  @host_names = YAML.load(File.read('./config/settings.yml'))['proxy_hosts'].uniq!
 
-  @host_names = File.open('./dump.csv', 'r:windows-1251').read.lines.drop(1)
-                .map { |line| SimpleIDN.to_ascii(line.split(';')[1].chomp('.')) } + additional_host_names
-  @host_names.uniq!
-
-  puts @host_names.last
   puts "Processing #{@host_names.count} host names"
 
   DYMAMIC_CONFIGS.each do |config_file|
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,36 +1,48 @@
+require 'mina/rails'
 require 'mina/git'
+# require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
+# require 'mina/rvm'    # for rvm support. (https://rvm.io)
 
-set :domain, '46.101.255.49'
+# Basic settings:
+#   domain       - The hostname to SSH to.
+#   deploy_to    - Path to deploy into.
+#   repository   - Git repo to clone from. (needed by mina/git)
+#   branch       - Branch name to deploy. (needed by mina/git)
+
+set :application_name, 'unlocker'
+set :domain, '138.197.178.66'
 set :user, 'root'
-set :repository, 'git@github.com:pavel-d/unlocker-configs.git'
 set :deploy_to, '/srv/unlocker-configs'
+set :repository, 'https://github.com/pavel-d/unlocker-configs.git'
+set :branch, 'master'
 set :forward_agent, true
 
+task :environment do
+end
+
+task :setup do
+end
+
+desc "Deploys the current version to the server."
 task :deploy do
   deploy do
     invoke :'git:clone'
-    invoke :fetch_dump
     invoke :render_config
     invoke :link_config
     invoke :restart
-    invoke :'deploy:cleanup'
   end
 end
 
-task :fetch_dump do
-  queue 'wget https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv'
-end
-
 task :render_config do
-  queue 'rake render_config'
+  command 'rake render_config'
 end
 
 task :link_config do
-  queue 'cp config/smart-proxy/* /etc/smart-proxy'
-  queue 'cp config/bind/* /etc/bind/'
+  command 'cp config/smart-proxy/* /etc/smart-proxy'
+  command 'cp config/bind/* /etc/bind/'
 end
 
 task :restart do
-  queue 'sv restart smart-proxy'
-  queue 'service bind9 reload'
+  command 'sv restart smart-proxy'
+  command 'service bind9 reload'
 end
